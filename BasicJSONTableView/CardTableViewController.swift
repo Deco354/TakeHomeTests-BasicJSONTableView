99059aa5b48bfb176f3f80d9d7167808479eadd8
Declan McKenna <deco354@gmail.com>
2020-06-01 09:56:45 +0100

    Connect Storyboard with TableView

--- BasicJSONTableView/CardTableViewController.swift
+++ BasicJSONTableView/CardTableViewController.swift
@@ -9,14 +9,38 @@
 import UIKit
 
 class CardTableViewController: UITableViewController {
+    
+    let networkController = NetworkController()
+    var cards = [Card]()
+    
+    override func viewDidLoad() {
+        networkController.request(from: URL(string: "https://deckofcardsapi.com/api/deck/new/draw/?count=52")!) { [weak self] result in
+            switch result {
+            case .success(let cards):
+                self?.cards = cards
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()                    
+                }
+            case .failure(let error):
+                print("Request Failed with error: \(error)")
+            }
+        }
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return cards.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CardCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CardCell", for: indexPath)
+        cell.textLabel?.text = cards[indexPath.row].value
+        return cell
     }
 }
