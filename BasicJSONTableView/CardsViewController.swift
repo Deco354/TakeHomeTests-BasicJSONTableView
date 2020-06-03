eee6f73cf120188cac7be416ebba939d3d486701
Declan McKenna <deco354@gmail.com>
2020-06-03 16:46:02 +0100

    Extract Networking code in to NetworkController

--- BasicJSONTableView/CardsViewController.swift
+++ BasicJSONTableView/CardsViewController.swift
@@ -10,57 +10,29 @@ import UIKit
 import Foundation
 
 class CardsViewController: UITableViewController {
+    var networking: NetworkController = NetworkController() //Change to use protocol
     var cards: [Card]?
     var cardImages = [UIImage?]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestCards { [weak self] result in
+        networking.requestCards { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let cards):
                     self?.cards = cards
-                    self?.cardImages = Array.init(repeating: nil, count: cards.count)
                     self?.tableView.reloadData()
-                    self?.downloadImages(from: cards.map{ $0.image })
+                    
+                    
+                    self?.cardImages = Array.init(repeating: nil, count: cards.count)
+                    for (index, card) in cards.enumerated() {
+                        self?.networking.downloadImage(from: card.image) { result in
+                            self?.cardImages[index] = try? result.get()
+                            DispatchQueue.main.async { self?.tableView.reloadData() }
+                        }
+                    }
                 case .failure(let error):
                     print(error)
-                }
-            }
-        }
-    }
-    
-    //move me to another class later
-    private func requestCards(completionHandler:@escaping (Result<[Card],NetworkCallError>) -> Void ) {
-        
-        let session = URLSession.shared
-        session.dataTask(with: URL(string: "https://deckofcardsapi.com/api/deck/new/draw/?count=52")!) { data, response, error in
-            guard let data = data else {
-                completionHandler(.failure(.noData))
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            do {
-                let deckOfCards = try decoder.decode(Cards.self, from: data)
-                completionHandler(.success(deckOfCards.cards))
-            } catch {
-                
-                completionHandler(.failure(.decodeFailure(error: error as! DecodingError)))
-            }
-        }.resume()
-    }
-    
-    //Todo: Handle errors
-    private func downloadImages(from urls: [URL]) {
-        DispatchQueue.global().async { [weak self] in
-            for (index, imageURL) in urls.enumerated() {
-                if let data = try? Data(contentsOf: imageURL),
-                    let image = UIImage(data: data) {
-                    self?.cardImages[index] = image
-                    DispatchQueue.main.async {
-                        self?.tableView.reloadData() //Find way to do this without reloading entire table each time
-                    }
                 }
             }
         }
---
+++ BasicJSONTableView/NetworkController.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkController.swift
+//  BasicJSONTableView
+//
+//  Created by Declan McKenna on 03/06/2020.
+//  Copyright © 2020 Declan McKenna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NetworkController {
+    let session: URLSession
+    
+    init(with session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func requestCards(completionHandler:@escaping (Result<[Card],NetworkCallError>) -> Void ) {
+        session.dataTask(with: URL(string: "https://deckofcardsapi.com/api/deck/new/draw/?count=52")!) { data, response, error in
+            guard let data = data else {
+                completionHandler(.failure(.noData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let deckOfCards = try decoder.decode(Cards.self, from: data)
+                completionHandler(.success(deckOfCards.cards))
+            } catch {
+                completionHandler(.failure(.decodeFailure(error: error as! DecodingError)))
+            }
+        }.resume()
+    }
+    
+    //Todo: Handle errors
+    func downloadImage(from url: URL, completionHandler: @escaping (Result<UIImage, NetworkCallError>) -> Void) {
+        DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: url),
+                    let image = UIImage(data: data) {
+                    completionHandler(.success(image))
+                }
+            }
+    }
+}
