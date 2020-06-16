3d12e45a7d0b9473b2c9315a059091b6d998f5f0
Declan McKenna <deco354@gmail.com>
2020-06-16 16:05:50 +0100

    Create JSONLoader
    A convenience api for loading JSON data within Unit Tests

---
+++ BasicJSONTableViewTests/Utilities/JSONLoader.swift
@@ -0,0 +1,20 @@
+//
+//  JSONLoading.swift
+//  BasicJSONTableViewTests
+//
+//  Created by Declan McKenna on 27/05/2020.
+//  Copyright © 2020 Declan McKenna. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    func jsonData(forResource resourceName: String) -> Data {
+        guard let url = self.url(forResource: resourceName, withExtension: "json"),
+        let data = try? Data(contentsOf: url)
+        else {
+            fatalError("\(resourceName).json could not be found")
+        }
+        return data
+    }
+}
