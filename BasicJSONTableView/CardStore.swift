7aa10a1ba012367baf46f8c3a3e6cc0ee2ec3252
Nick <ptnguyen1901@gmail.com>
2020-10-08 07:32:32 -0400

    refractor

--- BasicJSONTableView/CardStore.swift
+++ BasicJSONTableView/CardStore.swift
@@ -9,30 +9,30 @@
 import Foundation
 
 struct Card {
-  let code: String
-  let image: URL
-  let suit: String
-
-  enum CodingKeys: String, CodingKey {
-    case code
-    case image
-    case suit
-  }
+    let code: String
+    let image: URL
+    let suit: String
+    
+    enum CodingKeys: String, CodingKey {
+        case code
+        case image
+        case suit
+    }
 }
 
 extension Card: Decodable {
-  init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    let code = try container.decode(String.self, forKey: .code)
-    let imageStringURL = try container.decode(String.self, forKey: .image)
-    let suit = try container.decode(String.self, forKey: .suit)
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let code = try container.decode(String.self, forKey: .code)
+        let imageStringURL = try container.decode(String.self, forKey: .image)
+        let suit = try container.decode(String.self, forKey: .suit)
 
-    self.code = code
-    self.image = URL(string: imageStringURL)!
-    self.suit = suit
-  }
+        self.code = code
+        self.image = URL(string: imageStringURL)!
+        self.suit = suit
+    }
 }
 
 struct Cards: Decodable {
-  let cards: [Card]
+    let cards: [Card]
 }
